fb5666591637344a5d61edf95afc06e1e42702f3
sb5169 <sb5169@nyumc.org>
2014-11-25 10:52:33 -0500

    Package Update

--- R/nestedcv.R
+++ R/nestedcv.R
@@ -1,20 +1,3 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to Sana's Package, providing predictive modeling tools for your research. Hope you find it useful!")
-}
-
-
-#' Read dataframe file into R
-#'
-#' @param file A csv file with training dataframe that you want to read.
-#' @return Returns training dataframe
-#' @examples
-#' data<-readfile("train.csv")
-
-readfile<-function(file){
-  data<- read.csv(file, header = TRUE, stringsAsFactors=TRUE);
-}
-
-
 
 #' Run Nested Cross Validation on Training Data
 #'
@@ -62,4 +45,5 @@ nestedcv<- function(n,r,data,model,x)
   score<-mean(v)
   return(score)
 }
+
 
