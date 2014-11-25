a6ea6af96f133fa30b3b028793fb30f490b052be
sb5169 <sb5169@nyumc.org>
2014-11-25 14:02:42 -0500

    functons

---
+++ R/functions.R
@@ -0,0 +1,63 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to Sana's Package, providing predictive modeling tools for your research. Hope you find it useful!")
+}
+
+
+# #' Read dataframe file into R
+# #'
+# #' @param file A csv file with training dataframe that you want to read.
+# #' @return Returns training dataframe
+# #' @examples
+# #' data<-readfile("train.csv")
+
+readfile<-function(file){
+  data<- read.csv(file, header = TRUE, stringsAsFactors=TRUE);
+}
+
+# #' Run Nested Cross Validation on Training Data
+# #'
+# #' @param r Number of repeats for the nested cross validation code.
+# #' @param n Number of folds for the nested cross validation code.
+# #' @param data The dataframe with training data.
+# #' @param model Model used for nested cross validation code.
+# #' @param x Column name which represents the class you are trying to predict.
+# #' @return Returns the accuracy score of the nested cross validation code.
+# #' @examples
+# #' nestedcv(2,1,train,"rf","Survived")
+
+
+nestedcv<- function(n,r,data,model,x)
+{
+  trainData<-na.omit(data);
+  colnames(trainData)[which(names(trainData) == x)] <- "Class";
+  v <- numeric();
+  set.seed(500);
+
+  for(i in 1:r){
+    folds <- cvFolds(nrow(trainData), K=n);
+    for (j in 1:n){
+      Lset <- trainData[folds$subsets[folds$which != j], ];#Set the training set
+      expect_equal(object = nrow(Lset), expected = (nrow(trainData)/n) *(n-1), tolerance=.01);
+      Tset <- trainData[folds$subsets[folds$which == j], ];
+      expect_equal(object = nrow(Tset), expected = nrow(trainData)/n, tolerance=.01);
+      fitControl <- trainControl(method = "repeatedcv",
+                                 number = n,
+                                 repeats = r);
+      fit <- train(as.factor(Class) ~ .,
+                   data=Lset,
+                   na.action=na.omit,
+                   importance=TRUE,
+                   method=model,
+                   trControl = fitControl);
+      fit;
+      prediction<-predict(fit,newdata=Tset,na.action=na.omit);
+      expect_equal(object = length(prediction), expected = nrow(Tset));
+      cm<-confusionMatrix(data=prediction,Tset$Class);
+      accuracy <- cm$overall['Accuracy'];
+      v<-append(v,accuracy)
+    }
+  }
+  score<-mean(v)
+  return(score)
+}
+
